55df24dce7348a22c77738357df54b007410634e
Labeeb Mattra <mmlabeeb@gmail.com>
2015-09-02 23:04:16 +0530

    comic urls views

---
+++ comicmachine/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from models import ComicImage, ComicCollection
+
+
+def comicgen(request):
+    coll = ComicCollection.objects.get(name='Orion1')
+    coll1 = ComicImage.objects.filter(collection=coll)
+    context = {'coll1': coll1}
+    return render(request, 'comic/comicgen.html', context)
