76aa81a35569521336d775e84d39c95fb0c418b2
Labeeb Mattra <mmlabeeb@gmail.com>
2015-09-02 23:09:01 +0530

    comic acdmin

--- comic/admin.py
+++ comic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import ComicCollection, ComicImage
+
+
 
 # Register your models here.
+admin.site.register(ComicCollection)
+admin.site.register(ComicImage)
