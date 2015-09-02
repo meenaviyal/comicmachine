3475a3aa79623d1d22a9d6e431e1a74560e15e0e
Labeeb Mattra <mmlabeeb@gmail.com>
2015-09-02 23:35:57 +0530

    url fix and images added

--- comicmachine/urls.py
+++ comicmachine/urls.py
@@ -1,4 +1,4 @@
-"""comicmachine URL Configuration
+"""mememachine URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^images/', include('django_images.urls')),
+    url(r'^comic/', include('comic.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
