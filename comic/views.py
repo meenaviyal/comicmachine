772da6231b6627c7e4d27903f717f17068c74528
Labeeb Mattra <mmlabeeb@gmail.com>
2015-09-02 23:39:39 +0530

    fetch first collection (till beta)

--- comic/views.py
+++ comic/views.py
@@ -3,7 +3,7 @@ from models import ComicImage, ComicCollection
 
 
 def comicgen(request):
-    coll = ComicCollection.objects.get(name='Orion\'s Collection')
+    coll = ComicCollection.objects.first()
     coll1 = ComicImage.objects.filter(collection=coll)
     context = {'coll1': coll1}
     return render(request, 'comic/comicgen.html', context)
