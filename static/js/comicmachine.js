ddb33e80709c74417f1e70f26fdff4f02058704e
Labeeb Mattra <mmlabeeb@gmail.com>
2016-06-24 00:45:36 +0530

    splash screen added

--- static/js/comicmachine.js
+++ static/js/comicmachine.js
@@ -4,7 +4,90 @@
 //     // these HTTP methods do not require CSRF protection
 //     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 // }
-function imageToDataUri(img, width, height) {
+
+function toggleSplash() {
+
+        console.log("toggle start");
+        $('.rotatemodal').attr('style', 'display:block;');
+        // $('#splashDiv').hide();
+        var container = document.getElementById('container');
+        var drop = document.getElementById('drop');
+        var drop2 = document.getElementById('drop2');
+        var outline = document.getElementById('outline');
+
+        TweenMax.set(['svg'], {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            xPercent: -50,
+            yPercent: -50
+        })
+
+        TweenMax.set([container], {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            xPercent: -50,
+            yPercent: -50
+        })
+
+        TweenMax.set(drop, {
+            transformOrigin: '50% 50%'
+        })
+
+        var tl = new TimelineMax({
+            repeat: -1,
+            paused: false,
+            repeatDelay: 0,
+            immediateRender: false
+        });
+
+        tl.timeScale(3);
+
+        tl.to(drop, 4, {
+            attr: {
+                cx: 250,
+                rx: '+=10',
+                ry: '+=10'
+            },
+            ease: Back.easeInOut.config(3)
+        })
+        .to(drop2, 4, {
+            attr: {
+                cx: 250
+            },
+            ease: Power1.easeInOut
+        }, '-=4')
+        .to(drop, 4, {
+            attr: {
+                cx: 125,
+                rx: '-=10',
+                ry: '-=10'
+            },
+            ease: Back.easeInOut.config(3)
+        })
+        .to(drop2, 4, {
+            attr: {
+                cx: 125,
+                rx: '-=10',
+                ry: '-=10'
+            },
+            ease: Power1.easeInOut
+        }, '-=4')
+
+
+        
+        setTimeout(function() { 
+            $('.rotatemodal').attr('style', 'display:none;');
+        }, 3000);
+
+    return
+}
+
+
+ toggleSplash();
+
+ function imageToDataUri(img, width, height) {
 
     // create an off-screen canvas
     var tempcanvas = document.createElement('canvas'),
@@ -21,8 +104,13 @@ function imageToDataUri(img, width, height) {
     return tempcanvas.toDataURL();
 }
 
+
+$body = $("body");
 //document load
 $(function() {
+
+
+
     $('#editform').attr('style', 'color:white;');
     var csrftoken = Cookies.get('csrftoken');
     //   console.log("csrf token is");
@@ -150,6 +238,7 @@ $(function() {
             }
         }); //ajax
 
+        // toggleSplash(0);
 
     }; //fn get_images
 
