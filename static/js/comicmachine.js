1dc0ad170deeb4709765994b9c01ca7d5a852c5b
Labeeb Mattra <mmlabeeb@gmail.com>
2016-06-29 14:15:19 +0530

    Revert "nothing much comment"
    This reverts commit 0da43e7598c1f1e9add18a2144f60582572dbe57.

--- static/js/comicmachine.js
+++ static/js/comicmachine.js
@@ -171,10 +171,6 @@ $(function() {
                         "' alt=''></a></div>");
                 }); //each
 
-                // var m = new Masonry($('#libraryView').get()[0], {
-                //     itemSelector: ".thumb"
-                // });
-
                 $('.img-responsive').hover(function() {
                     $(this).addClass('transition');
 
