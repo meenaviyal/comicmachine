ae299a183cd224ba1809eb830ab9c7f925cbde26
Labeeb Mattra <mmlabeeb@gmail.com>
2016-07-02 12:55:06 +0530

    Splash UI bad alignment fix fix.

--- static/js/comicmachine.js
+++ static/js/comicmachine.js
@@ -6,79 +6,7 @@
 // }
 
 function toggleSplash() {
-    $('#splashbubble').show();
-    $('.rotatemodal').attr('style', 'display:block;');
-    // $('#splashDiv').hide();
-    var container = document.getElementById('container');
-    var drop = document.getElementById('drop');
-    var drop2 = document.getElementById('drop2');
-    var outline = document.getElementById('outline');
 
-    TweenMax.set(['svg'], {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        xPercent: -50,
-        yPercent: -50
-    })
-
-    TweenMax.set([container], {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        xPercent: -50,
-        yPercent: -50
-    })
-
-    TweenMax.set(drop, {
-        transformOrigin: '50% 50%'
-    })
-
-    var tl = new TimelineMax({
-        repeat: -1,
-        paused: false,
-        repeatDelay: 0,
-        immediateRender: false
-    });
-
-    tl.timeScale(3);
-
-    tl.to(drop, 4, {
-            attr: {
-                cx: 250,
-                rx: '+=10',
-                ry: '+=10'
-            },
-            ease: Back.easeInOut.config(3)
-        })
-        .to(drop2, 4, {
-            attr: {
-                cx: 250
-            },
-            ease: Power1.easeInOut
-        }, '-=4')
-        .to(drop, 4, {
-            attr: {
-                cx: 125,
-                rx: '-=10',
-                ry: '-=10'
-            },
-            ease: Back.easeInOut.config(3)
-        })
-        .to(drop2, 4, {
-            attr: {
-                cx: 125,
-                rx: '-=10',
-                ry: '-=10'
-            },
-            ease: Power1.easeInOut
-        }, '-=4')
-
-
-
-    setTimeout(function() {
-        $('.rotatemodal').attr('style', 'display:none;');
-    }, 3000);
 
     return
 }
